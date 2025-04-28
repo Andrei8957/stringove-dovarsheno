08e1d1356016575be1f849f44d050028ef9e6b18
Andrei8957 <andrejdelevski@example.com>
2025-04-28 23:44:29 +0300

    Add project files.

---
+++ ot desetichna w n ichna/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ot_desetichna_w_n_ichna
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ stringove/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stringove
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("wywedi chisloto w koqto iskash da preobrazuwash i wywedi chisloto koeto she preobrazuwash");
+            string[] nachalo=Console.ReadLine().Split(' ').ToArray();
+            int izhodnoChislo=int.Parse(nachalo[0]);
+            int chisloZApreobrazuvane=int.Parse(nachalo[1]);
+            string gotovoChislo = "";
+
+           while(chisloZApreobrazuvane>0)
+            {
+                int ostatyk= chisloZApreobrazuvane%izhodnoChislo;
+                gotovoChislo = ostatyk + gotovoChislo;
+                chisloZApreobrazuvane = chisloZApreobrazuvane / izhodnoChislo;
+            }
+            Console.WriteLine(gotovoChislo);
+        }
+    }
+}
